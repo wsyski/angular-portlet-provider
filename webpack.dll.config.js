fde41d199d2e1a7735722bee0574120f8b92e29a
wsyski <wsyski@gmail.com>
2019-12-29 17:14:35 +0100

    Initial version

---
+++ webpack.dll.config.js
@@ -0,0 +1,37 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  optimization: {
+    minimize: false,
+    noEmitOnErrors: true
+  },
+  entry: {
+    polyfills: [
+      'zone.js/dist/zone'
+    ],
+    vendor: [
+      "@angular/animations",
+      "@angular/common",
+      "@angular/core",
+      "@angular/forms",
+      "@angular/platform-browser",
+      "@angular/platform-browser-dynamic",
+      "@angular/router",
+      "rxjs"
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dll'),
+    filename: "[name]-dll-es5.js",
+    library: "[name]"
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      path: path.resolve("dll", "[name]-manifest.json"),
+      name: "[name]",
+      entryOnly: true
+    })
+  ]
+};
